29cb56e051a8dba218e454f003febc384512cd9f
post-coder <gabrielspanu96@gmail.com>
2023-03-22 11:06:24 +0100

    milestone 5

--- js/script.js
+++ js/script.js
@@ -21,17 +21,23 @@ createApp({
                     {
                         date: '10/01/2020 15:30:55',
                         message: 'Hai portato a spasso il cane?',
-                        status: 'sent'
+                        status: 'sent',
+                        // controlla se visualizzare il menu
+                        showMenu: false,
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         message: 'Ricordati di stendere i panni',
-                        status: 'sent'
+                        status: 'sent',
+                        // controlla se visualizzare il menu
+                        showMenu: false,
                     },
                     {
                         date: '10/01/2020 16:15:22',
                         message: 'Tutto fatto!',
-                        status: 'received'
+                        status: 'received',
+                        // controlla se visualizzare il menu
+                        showMenu: false,
                     },
                 ],
             },
@@ -43,17 +49,23 @@ createApp({
                     {
                         date: '20/03/2020 16:30:00',
                         message: 'Ciao come stai?',
-                        status: 'sent'
+                        status: 'sent',
+                        // controlla se visualizzare il menu
+                        showMenu: false,
                     },
                     {
                         date: '20/03/2020 16:30:55',
                         message: 'Bene grazie! Stasera ci vediamo?',
-                        status: 'received'
+                        status: 'received',
+                        // controlla se visualizzare il menu
+                        showMenu: false,
                     },
                     {
                         date: '20/03/2020 16:35:00',
                         message: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                        status: 'sent'
+                        status: 'sent',
+                        // controlla se visualizzare il menu
+                        showMenu: false,
                     }
                 ],
             },
@@ -65,17 +77,23 @@ createApp({
                     {
                         date: '28/03/2020 10:10:40',
                         message: 'La Marianna va in campagna',
-                        status: 'received'
+                        status: 'received',
+                        // controlla se visualizzare il menu
+                        showMenu: false,
                     },
                     {
                         date: '28/03/2020 10:20:10',
                         message: 'Sicuro di non aver sbagliato chat?',
-                        status: 'sent'
+                        status: 'sent',
+                        // controlla se visualizzare il menu
+                        showMenu: false,
                     },
                     {
                         date: '28/03/2020 16:15:22',
                         message: 'Ah scusa!',
-                        status: 'received'
+                        status: 'received',
+                        // controlla se visualizzare il menu
+                        showMenu: false,
                     }
                 ],
             },
@@ -87,12 +105,16 @@ createApp({
                     {
                         date: '10/01/2020 15:30:55',
                         message: 'Lo sai che ha aperto una nuova pizzeria?',
-                        status: 'sent'
+                        status: 'sent',
+                        // controlla se visualizzare il menu
+                        showMenu: false,
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         message: 'Si, ma preferirei andare al cinema',
-                        status: 'received'
+                        status: 'received',
+                        // controlla se visualizzare il menu
+                        showMenu: false,
                     }
                 ],
             },
@@ -104,12 +126,16 @@ createApp({
                     {
                         date: '10/01/2020 15:30:55',
                         message: 'Ricordati di chiamare la nonna',
-                        status: 'sent'
+                        status: 'sent',
+                        // controlla se visualizzare il menu
+                        showMenu: false,
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         message: 'Va bene, stasera la sento',
-                        status: 'received'
+                        status: 'received',
+                        // controlla se visualizzare il menu
+                        showMenu: false,
                     }
                 ],
             },
@@ -121,17 +147,23 @@ createApp({
                     {
                         date: '10/01/2020 15:30:55',
                         message: 'Ciao Claudia, hai novità?',
-                        status: 'sent'
+                        status: 'sent',
+                        // controlla se visualizzare il menu
+                        showMenu: false,
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         message: 'Non ancora',
-                        status: 'received'
+                        status: 'received',
+                        // controlla se visualizzare il menu
+                        showMenu: false,
                     },
                     {
                         date: '10/01/2020 15:51:00',
                         message: 'Nessuna nuova, buona nuova',
-                        status: 'sent'
+                        status: 'sent',
+                        // controlla se visualizzare il menu
+                        showMenu: false,
                     }
                 ],
             },
@@ -143,12 +175,16 @@ createApp({
                     {
                         date: '10/01/2020 15:30:55',
                         message: 'Fai gli auguri a Martina che è il suo compleanno!',
-                        status: 'sent'
+                        status: 'sent',
+                        // controlla se visualizzare il menu
+                        showMenu: false,
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         message: 'Grazie per avermelo ricordato, le scrivo subito!',
-                        status: 'received'
+                        status: 'received',
+                        // controlla se visualizzare il menu
+                        showMenu: false,
                     }
                 ],
             },
@@ -160,17 +196,23 @@ createApp({
                     {
                         date: '10/01/2020 15:30:55',
                         message: 'Ciao, andiamo a mangiare la pizza stasera?',
-                        status: 'received'
+                        status: 'received',
+                        // controlla se visualizzare il menu
+                        showMenu: false,
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
-                        status: 'sent'
+                        status: 'sent',
+                        // controlla se visualizzare il menu
+                        showMenu: false,
                     },
                     {
                         date: '10/01/2020 15:51:00',
                         message: 'OK!!',
-                        status: 'received'
+                        status: 'received',
+                        // controlla se visualizzare il menu
+                        showMenu: false,
                     }
                 ],
             }
@@ -184,10 +226,52 @@ createApp({
 
         // proprietà che memorizza la ricerca dell'utente tra i contatti
         userSearch: '',
+
+        now: {},
+
     }
   },
 
+  mounted() {
+
+    const dt = luxon.DateTime.now();
+    this.now = dt;
+
+
+    // mostra l'orario del primo messaggio del primo account
+    
+    console.log(luxon.DateTime.fromFormat(this.contacts[0].messages[0].date, "dd/MM/yyyy hh:mm:ss"));
+
+    this.contacts.forEach((contact) => {
+
+        contact.messages.forEach((message) => {
+            let luxonDate = luxon.DateTime.fromFormat(message.date, "dd/MM/yyyy hh:mm:ss");
+            message.date = luxonDate.hour + ":" + luxonDate.minute;
+        });
+
+    });
+
+  },
+
   methods: {
+
+    deleteMessage(messageIndex) {
+        // cancellare il messaggio selezionato dall'utente
+
+        // con activeContactIndex sappiamo quale sia la CHAT aperta dall'utente
+
+        // in qualche modo dovremo fare uno splice dell'array dei messaggi del contatto
+        // ci serve l'index del messaggio da cancellare
+        
+        // il metodo splice va applicato ad un array, indicando l'indice 
+        this.contacts[this.activeContactIndex].messages.splice(messageIndex, 1);
+
+    },
+
+    toggleShowMenu(singleMessage) {
+        // invertiamo la variabile booleana
+        singleMessage.showMenu = !singleMessage.showMenu;
+    },
     
     changeActiveContactIndex(contactIndex) {
       // cambiare la proprietà activeContactIndex
@@ -203,7 +287,7 @@ createApp({
         
         // creare un nuovo messaggio
         const newMessage = {
-            date: '10/01/2020 15:51:00',
+            date: this.now.hour + ':' + this.now.minute,
             message: this.userMessageInput,
             status: 'sent',
         };
@@ -217,7 +301,7 @@ createApp({
 
         // mi aspetto una risposta dal contatto
         const contactReply = {
-            date: '10/01/2020 15:51:00',
+            date: this.now.hour + ':' + this.now.minute,
             message: 'ok',
             status: 'received',
         }
